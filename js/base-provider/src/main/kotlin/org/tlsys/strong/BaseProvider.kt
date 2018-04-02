4ae53b7eece9c9b967448b3d111fc3b37b54275c
Subochev <caffeine.mgn@gmail.com>
2018-04-02 23:29:57 +0300

    Добавил сборку в JS

---
+++ js/base-provider/src/main/kotlin/org/tlsys/strong/BaseProvider.kt
@@ -0,0 +1,101 @@
+package org.tlsys.strong
+
+import kotlin.reflect.KClass
+import kotlin.reflect.KProperty
+
+/**
+ * Базовый провайдер
+ *
+ * Поддерживает три типа работы бизнес-объекта:
+ * Singleton
+ * Stateless
+ * Stateful
+ */
+open class BaseProvider : StrongProvider {
+
+    enum class ScopeType {
+        STATELESS,
+        STATEFUL,
+        SINGLETON
+    }
+
+    internal interface IRecord<T : Any> {
+        fun getInject(): StrongProvider.Injector<T>
+    }
+
+    internal class StatelessRecord<T : Any>(val f: () -> T) : IRecord<T> {
+        val inj = object : StrongProvider.Injector<T> {
+            override fun getBean(): T = f()
+        }
+
+        override fun getInject(): StrongProvider.Injector<T> = inj
+    }
+
+    internal inner class SingletonRecord<T : Any>(val f: () -> T) : IRecord<T> {
+        override fun getInject(): StrongProvider.Injector<T> = inj
+        private var imp: T? = null
+
+        val inj = object : StrongProvider.Injector<T> {
+            override fun getBean(): T {
+                return synchronized(this@BaseProvider) {
+                    if (imp == null) {
+                        imp = f()
+                    }
+                    imp!!
+                }
+            }
+        }
+
+
+    }
+
+    internal class StatefulRecord<T : Any>(val f: () -> T) : IRecord<T> {
+        override fun getInject(): StrongProvider.Injector<T> = object : StrongProvider.Injector<T> {
+            private var imp: T? = null
+            override fun getBean(): T {
+                if (imp == null)
+                    imp = f()
+                return imp!!
+            }
+        }
+    }
+
+    private val factory = HashMap<KClass<*>, IRecord<*>>()
+
+
+
+    fun <T : Any> bind(interfaces: Set<KClass<*>>, impClass: KClass<T>, scope: ScopeType, imp: () -> T) {
+        val record = when (scope) {
+            ScopeType.SINGLETON -> SingletonRecord(imp)
+            ScopeType.STATELESS -> StatelessRecord(imp)
+            ScopeType.STATEFUL -> StatefulRecord(imp)
+        }
+
+        interfaces.forEach {
+            if (factory.containsKey(it)) TODO()
+        }
+        interfaces.forEach {
+            factory[it] = record
+        }
+    }
+
+    inline fun <reified T : Any> bind(vararg classes: KClass<*>, scope: ScopeType, noinline imp: () -> T) {
+        bind(interfaces = classes.toSet(), impClass = T::class, imp = imp, scope = scope)
+    }
+
+    inline fun <reified T : Any> stateless(vararg classes: KClass<*>, noinline imp: () -> T) {
+        bind(interfaces = classes.toSet(), impClass = T::class, imp = imp, scope = ScopeType.STATELESS)
+    }
+
+    inline fun <reified T : Any> stateful(vararg classes: KClass<*>, noinline imp: () -> T) {
+        bind(interfaces = classes.toSet(), impClass = T::class, imp = imp, scope = ScopeType.STATEFUL)
+    }
+
+    inline fun <reified T : Any> singleton(vararg classes: KClass<*>, noinline imp: () -> T) {
+        bind(interfaces = classes.toSet(), impClass = T::class, imp = imp, scope = ScopeType.SINGLETON)
+    }
+
+    override fun <T : Any> getInjector(clazz: KClass<T>, property: KProperty<T>): StrongProvider.Injector<T>? =
+            factory[clazz]?.getInject() as StrongProvider.Injector<T>
+
+}
